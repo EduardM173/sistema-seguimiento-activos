e9e33f5ae16dc3f7d24fa1306af341cf97bacd3e
EduardM173 <eduardo.apaza@ucb.edu.bo>
2026-03-26 13:59:28 -0400

    arreglo de bugs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,14 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { AuthProvider } from './context/AuthContext';
-
 import ProtectedRoute from './components/auth/ProtectedRoute';
-
 import PrivateLayout from './components/layout/PrivateLayout';
 
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
 import UserList from './pages/users/UserList';
 import CreateUser from './components/users/CreateUser';
+import AssetsPage from './pages/AssetsPage';
 
 export default function App() {
   return (
@@ -20,12 +19,19 @@ export default function App() {
         <Route element={<ProtectedRoute />}>
           <Route element={<PrivateLayout />}>
             <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/users" element={<UserList />} />
-            <Route path="/users/create" element={<CreateUser />} />
+
+            <Route element={<ProtectedRoute requiredPermission="ASSET_VIEW" />}>
+              <Route path="/assets" element={<AssetsPage />} />
+            </Route>
+
+            <Route element={<ProtectedRoute requiredPermission="USER_MANAGE" />}>
+              <Route path="/users" element={<UserList />} />
+              <Route path="/users/create" element={<CreateUser />} />
+            </Route>
           </Route>
         </Route>
 
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </AuthProvider>
   );
